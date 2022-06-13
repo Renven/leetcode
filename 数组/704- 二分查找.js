3afac13ecb6da88f66918003df34a9e7ba7d396b
Renven <renven666@gmail.com>
2022-06-13 23:33:10 +0800

    [Array-27-移除元素]

--- 数组/704- 二分查找.js
+++ 数组/704- 二分查找.js
@@ -34,14 +34,17 @@ nums 的每个元素都将在 [-9999, 9999]之间。
 // }
 
 var search = function (nums, target) {
+    // right是数组最后一个数的下标，num[right]在查找范围内，是左闭右闭区间
     let left = 0;
     let right = nums.length - 1;
+    // 当left=right时，由于nums[right]在查找范围内，所以要包括此情况
     while (left <= right) {
         let mid = left + Math.floor((right - left) / 2);
+        // 如果中间数大于目标值，要把中间数排除查找范围，所以右边界更新为mid-1；如果右边界更新为mid，那中间数还在下次查找范围内
         if (nums[mid] > target) {
-            right = mid - 1;
+            right = mid - 1; // 去左面闭区间寻找
         } else if (nums[mid] < target) {
-            left = mid + 1;
+            left = mid + 1; // 去右面闭区间寻找
         } else {
             return mid;
         }
