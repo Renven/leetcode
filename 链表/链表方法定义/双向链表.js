e1defe96c146820e685c37cac577aab15ddaae2b
Renven <renven666@gmail.com>
2022-07-11 15:11:10 +0800

    [update:README&&链表方法实现]

--- 链表/链表方法定义/双向链表.js
+++ 链表/链表方法定义/双向链表.js
@@ -68,4 +68,153 @@ function DoublyLinkedList() {
 
         return resultString
     }
+
+    //3.insert方法
+    DoublyLinkedList.prototype.insert = function (position, data) {
+        //1.越界判断
+        if (position < 0 || position > this.length) return false
+
+        //2.根据data创建新的节点
+        var newNode = new Node(data)
+
+        //3.判断原来的列表是否为空
+        if (this.length == 0) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            if (position == 0) { // 3.1. 判断position是否为0
+                this.head.prev = newNode
+                newNode.next = this.head
+                this.head = newNode
+            } else if (position == this.length) {
+                newNode.prev = this.tail
+                this.tail.next = newNode
+                this.tail = newNode
+            } else {
+                var current = this.head
+                var index = 0
+
+                while (index++ < position) {
+                    current = current.next
+                }
+
+                //修改指针
+                newNode.next = current
+                newNode.prev = current.prev
+                current.prev.next = newNode
+                current.prev = newNode
+            }
+        }
+
+        // 4.length+1
+        this.length += 1
+
+        return true
+    }
+
+    // 4.get方法
+    DoublyLinkedList.prototype.get = function (position) {
+        // 1.越界判断
+        if (position < 0 || position >= this.length) return null
+
+        //2.获取元素
+        var current = this.head
+        var index = 0
+
+        while (index++ < position) {
+            current = current.next
+        }
+
+        return current.data
+    }
+
+    // 5.indexOf方法
+    DoublyLinkedList.prototype.indexOf = function (data) {
+        // 1.定义变量
+        var current = this.head
+        var index = 0
+
+        //2.查找和data相同的节点
+        while (current) {
+            if (current.data == data) {
+                return index
+            }
+            current = current.next
+            index += 1
+        }
+        return -1
+    }
+
+    // 6.update方法
+    DoublyLinkedList.prototype.update = function (position, newData) {
+        // 1.越界的判断
+        if (position < 0 || position >= this.length) return false
+
+        // 2.寻找正确的节点
+        var current = this.head
+        var index = 0
+        while (index++ < position) {
+            current = current.next
+        }
+
+        // 3.修改找到节点的data信息
+        current.data = newData
+
+        return true
+    }
+
+    // 7.removeAt方法
+    DoublyLinkedList.prototype.removeAt = function (position) {
+        // 1.越界判断
+        if (position < 0 || position >= this.length) return null
+
+        // 2.判断是否只有一个节点
+        var current = this.head
+        if (this.length == 1) {
+            this.head = null
+            this.tail = null
+        } else {
+            if (position == 0) { //判断是否删除的是第一个节点
+                this.head.next.prev = null
+                this.head = this.head.next
+            } else if (position == this.length - 1) { //最后节点
+                current = this.tail
+                this.tail.prev.next = null
+                this.tail = this.tail.prev
+            } else {
+                var index = 0
+
+                while (index++ < position) {
+                    current = current.next
+                }
+
+                current.prev.next = current.next
+                current.next.prev = current.prev
+            }
+        }
+
+        // 3.length -1
+        this.length -= 1
+
+        return current.data
+    }
+
+    // 8.remove方法
+    DoublyLinkedList.prototype.remove = function (data) {
+        // 1.根据data获取下标值
+        var index = this.indexOf(data)
+
+        // 2.根据index删除对应位置的节点
+        return this.removeAt(index)
+    }
+
+    // 9.isEmpty方法
+    DoublyLinkedList.prototype.isEmpty = function () {
+        return this.length == 0
+    }
+
+    // 10.size方法
+    DoublyLinkedList.prototype.size = function () {
+        return this.length
+    }
 }
