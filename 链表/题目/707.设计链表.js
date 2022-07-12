8716bbd37f66d156c6654710cc876df6cc13d883
Renven <renven666@gmail.com>
2022-07-12 10:35:32 +0800

    [add:707.设计链表]

--- 链表/题目/707.设计链表.js
+++ 链表/题目/707.设计链表.js
@@ -9,39 +9,17 @@
  * addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val  的节点。如果 index 等于链表的长度，则该节点将附加到链表的末尾。如果 index 大于链表长度，则不会插入节点。如果index小于0，则在头部插入节点。
  * deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。
  */
-class LinkNode {
-    constructor(val, next) {
-        this.val = val;
-        this.next = next;
-    }
-}
 
 var MyLinkedList = function () {
-    this._size = 0;
-    this._tail = null;
-    this._head = null;
+
 };
 
 /** 
  * @param {number} index
  * @return {number}
  */
-
-MyLinkedList.prototype.getNode = function (index) {
-    if (index < 0 || index >= this._size) return null;
-    // 创建虚拟头节点
-    let cur = new LinkNode(0, this._head);
-    // 0 -> head
-    while (index-- >= 0) {
-        cur = cur.next;
-    }
-    return cur;
-};
-
 MyLinkedList.prototype.get = function (index) {
-    if (index < 0 || index >= this._size) return -1;
-    // 获取当前节点
-    return this.getNode(index).val;
+
 };
 
 /** 
@@ -49,12 +27,7 @@ MyLinkedList.prototype.get = function (index) {
  * @return {void}
  */
 MyLinkedList.prototype.addAtHead = function (val) {
-    const node = new LinkNode(val, this._head);
-    this._head = node;
-    this._size++;
-    if (!this._tail) {
-        this._tail = node;
-    }
+
 };
 
 /** 
@@ -62,15 +35,7 @@ MyLinkedList.prototype.addAtHead = function (val) {
  * @return {void}
  */
 MyLinkedList.prototype.addAtTail = function (val) {
-    const node = new LinkNode(val, null);
-    this._size++;
-    if (this._tail) {
-        this._tail.next = node;
-        this._tail = node;
-        return;
-    }
-    this._tail = node;
-    this._head = node;
+
 };
 
 /** 
@@ -79,19 +44,7 @@ MyLinkedList.prototype.addAtTail = function (val) {
  * @return {void}
  */
 MyLinkedList.prototype.addAtIndex = function (index, val) {
-    if (index > this._size) return;
-    if (index <= 0) {
-        this.addAtHead(val);
-        return;
-    }
-    if (index === this._size) {
-        this.addAtTail(val);
-        return;
-    }
-    // 获取目标节点的上一个的节点
-    const node = this.getNode(index - 1);
-    node.next = new LinkNode(val, node.next);
-    this._size++;
+
 };
 
 /** 
@@ -99,24 +52,7 @@ MyLinkedList.prototype.addAtIndex = function (index, val) {
  * @return {void}
  */
 MyLinkedList.prototype.deleteAtIndex = function (index) {
-    if (index < 0 || index >= this._size) return;
-    if (index === 0) {
-        this._head = this._head.next;
-        // 如果删除的这个节点同时是尾节点，要处理尾节点
-        if (index === this._size - 1) {
-            this._tail = this._head
-        }
-        this._size--;
-        return;
-    }
-    // 获取目标节点的上一个的节点
-    const node = this.getNode(index - 1);
-    node.next = node.next.next;
-    // 处理尾节点
-    if (index === this._size - 1) {
-        this._tail = node;
-    }
-    this._size--;
+
 };
 
 /**
